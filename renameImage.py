2074c2f47b231451122b8c988e82681280d6e29f
fastalways <pongsagorn.ch@gmail.com>
2022-12-12 15:47:14 +0700

    continue renameImage Code

--- renameImage.py
+++ renameImage.py
@@ -3,7 +3,7 @@ import glob
 
 ## -------------- CONFIG HERE ---------------------
 DIR = "E:/eieie/"
-ALLOW_IMAGE_TYPE = ['png','TXt']
+ALLOW_IMAGE_TYPE = ['txt']
 ## ------------------------------------------------
 
 
@@ -11,12 +11,10 @@ filenames = []
 imagenames = []
 for IMAGE_TYPE in ALLOW_IMAGE_TYPE:
     filenames.append(glob.glob(DIR+"*."+IMAGE_TYPE))
-    print(IMAGE_TYPE)
-    print(len(filenames[len(filenames)-1]))
+    print(f"{IMAGE_TYPE} has {len(filenames[len(filenames)-1])}.")
 for names in filenames :
     for name in names:
         imagenames.append(name) 
-print(f"ALL Image = {len(imagenames)}")
 imagenames.sort()
 if(len(imagenames)>0):
     imgpath = os.path.split(imagenames[0])[0]
@@ -27,9 +25,28 @@ if(len(imagenames)>0):
         split_path=imgpath.split("//")
     folderName = split_path[len(split_path)-1]
     print(f"Rename to -> {folderName}")
-for count,imagename in enumerate(imagenames):
-    imgpath = os.path.split(imagename)[0]
-    imgname = os.path.split(imagename)[1]
-    file_extension = os.path.splitext(imgname)[1]
-    newname = folderName + "_" + str(count+1).zfill(5) + file_extension
-    print(imgpath+"/"+newname)
+    print(f"Examples of renamed files : ")
+    for count,imagename in enumerate(imagenames):
+        imgpath = os.path.split(imagename)[0]
+        imgname = os.path.split(imagename)[1]
+        file_extension = os.path.splitext(imgname)[1]
+        newname = folderName + "_" + str(count+1).zfill(5) + file_extension
+        print(imgpath+"/"+newname)
+        if count == 4:
+            break
+    # Confirmation
+    all_images_count = len(imagenames)
+    print(f"ALL Image = {all_images_count}")
+    confFlag = input("Confirm to rename? (y/n) ")
+    if(confFlag=='y' or confFlag=='Y'):
+        for count,imagename in enumerate(imagenames):
+            imgpath = os.path.split(imagename)[0]
+            imgname = os.path.split(imagename)[1]
+            file_extension = os.path.splitext(imgname)[1]
+            newname = folderName + "_" + str(count+1).zfill(5) + file_extension
+            os.rename(imagename,imgpath+'/'+newname)
+            if count%49==0:
+                print(f"{count+1}/{all_images_count}")
+        print("Done!")
+else:
+    print(f"no image type {ALLOW_IMAGE_TYPE} in {DIR}")
